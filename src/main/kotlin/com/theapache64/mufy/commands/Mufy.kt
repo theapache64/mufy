0e10986d30d156c90fd7ad1fd0dc959e25624005
theapache64 <theapache64@gmail.com>
2020-05-16 18:09:06 +0530

    ➕ add main command

---
+++ src/main/kotlin/com/theapache64/mufy/commands/Mufy.kt
@@ -0,0 +1,12 @@
+package com.theapache64.mufy.commands
+
+import picocli.CommandLine
+
+@CommandLine.Command(
+    name = "mufy",
+    version = ["v1.0.0-alpha01"],
+    mixinStandardHelpOptions = true
+)
+class Mufy {
+
+}
