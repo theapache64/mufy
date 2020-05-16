53a7a194784be87c2e89f8dd83f2b1debcdabbe3
theapache64 <theapache64@gmail.com>
2020-05-16 18:31:47 +0530

    ◀️ start TDD

--- src/main/kotlin/com/theapache64/mufy/commands/Mufy.kt
+++ src/main/kotlin/com/theapache64/mufy/commands/Mufy.kt
@@ -1,12 +1,24 @@
 package com.theapache64.mufy.commands
 
+import com.theapache64.cyclone.core.base.BaseCommand
 import picocli.CommandLine
+import javax.inject.Inject
+import javax.inject.Singleton
 
 @CommandLine.Command(
     name = "mufy",
     version = ["v1.0.0-alpha01"],
     mixinStandardHelpOptions = true
 )
-class Mufy {
+@Singleton
+class Mufy constructor(isFromTest: Boolean = false) : BaseCommand<Int>(isFromTest) {
+
+    companion object {
+        const val GIFS_GENERATED = 200
+    }
+
+    override fun call(): Int {
+        TODO("Not yet implemented")
+    }
 
 }
---
+++ src/test/kotlin/com/theapache64/mufy/commands/MufyTest.kt
@@ -0,0 +1,28 @@
+package com.theapache64.mufy.commands
+
+import com.winterbe.expekt.should
+import org.junit.Before
+import org.junit.Test
+import picocli.CommandLine
+import java.io.PrintWriter
+import java.io.StringWriter
+
+
+class MufyTest {
+
+    private lateinit var mufyCmd: CommandLine
+    private val mufy = Mufy(true)
+
+
+    @Before
+    fun setUp() {
+        this.mufyCmd = CommandLine(mufy)
+        this.mufyCmd.out = PrintWriter(StringWriter())
+    }
+
+    @Test
+    fun `Generate gifs`() {
+        val exitCode = mufyCmd.execute("-i /home/theapache64/Documents/projects/mufy/lab/movie.mp4 -n 3 -k \"What\"")
+        exitCode.should.equal(Mufy.GIFS_GENERATED)
+    }
+}
